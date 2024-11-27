f231947fadd6b4ea1ac71308f7194e3d19397b4f
Earth34r <delaware.wade@gmail.com>
2024-11-27 12:25:03 -0800

    Run biome check

--- src/tree/tree_two.js
+++ src/tree/tree_two.js
@@ -1,14 +1,14 @@
-import getSeededRandom from './seededRandom.js'
+import getSeededRandom from "./seededRandom.js";
 
 // Make an instance of two and place it on the page.
-var params = {
-  fullscreen: true,
-  autostart: true
+const params = {
+	fullscreen: true,
+	autostart: true,
 };
-var elem = document.body;
-var two = new Two(params).appendTo(elem);
+const elem = document.body;
+const two = new Two(params).appendTo(elem);
 
-var rand = getSeededRandom('acba');
+let rand = getSeededRandom("acba");
 const randSI = (min = 0, max = 1) => Math.floor(rand() * (max - min + 1)) + min;
 const randS = (min = 0, max = 1) => rand() * (max - min) + min;
 
@@ -30,123 +30,129 @@ const widthTwig = 3;
 const windX = -1;
 const windY = 0;
 const bendability = 8;
-const windStrength = 0.01 * bendability * ((200 ** 2) / (two.height ** 2));
+const windStrength = 0.01 * bendability * (200 ** 2 / two.height ** 2);
 const windBendRectSpeed = 0.01;
 const windBranchSpring = 0.98;
-const gustProbability = 1/100;
+const gustProbability = 1 / 100;
 
-var windCycle = 0;
-var windCycleGust = 0;
-var windCycleGustTime = 0;
-var currentWind = 0;
-var windFollow = 0;
-var windActual = 0;
-var treeSeed = Math.random() * 10000 | 0;
-var branchCount = 0;
-var maxTrunk = 0;
-var treeGrow = 0.1;
+let windCycle = 0;
+let windCycleGust = 0;
+let windCycleGustTime = 0;
+let currentWind = 0;
+let windFollow = 0;
+let windActual = 0;
+let treeSeed = (Math.random() * 10000) | 0;
+let branchCount = 0;
+let maxTrunk = 0;
+let treeGrow = 0.1;
 
 function drawLeaf(x, y, dir, size) {
-    const actualSize = size * (1 + randS(-leafVariation, leafVariation));
-    var leaf = two.makeSprite("./tree/leaf.png", x, y);
-    leaf.rotation = (dir + Math.PI / 2 + randS(-1, 1)); // Add slight random rotation
-    leaf.scale = actualSize / 20
+	const actualSize = size * (1 + randS(-leafVariation, leafVariation));
+	const leaf = two.makeSprite("./tree/leaf.png", x, y);
+	leaf.rotation = dir + Math.PI / 2 + randS(-1, 1); // Add slight random rotation
+	leaf.scale = actualSize / 20;
 }
 
 function drawTree(seed) {
-    branchCount = 0;
-    treeGrow += 0.02;
-    rand = getSeededRandom(seed.toString());
-    maxTrunk = randSI(trunkMin, trunkMax);
-    drawBranch(two.width / 2, two.height, -Math.PI / 2, two.height / 4, maxTrunk);
+	branchCount = 0;
+	treeGrow += 0.02;
+	rand = getSeededRandom(seed.toString());
+	maxTrunk = randSI(trunkMin, trunkMax);
+	drawBranch(two.width / 2, two.height, -Math.PI / 2, two.height / 4, maxTrunk);
 }
 
-
 function drawBranch(x, y, dir, leng, width) {
-    branchCount++;
-    const treeGrowVal = (treeGrow > 1 ? 1 : treeGrow < 0.1 ? 0.1 : treeGrow) ** 2;
-    
-    const xx = Math.cos(dir) * leng * treeGrowVal;
-    const yy = Math.sin(dir) * leng * treeGrowVal;
-    const windSideWayForce = windX * yy - windY * xx;
-    
-    dir += (windStrength * windActual) * ((1 - width / maxTrunk) ** bendability) * windSideWayForce;
-    
-    const endX = x + Math.cos(dir) * leng * treeGrowVal;
-    const endY = y + Math.sin(dir) * leng * treeGrowVal;
-    var branch = two.makeLine(x, y, endX, endY)
-    branch.linewidth = width;
-    branch.fill = 'brown';
-    branch.stroke = '#4B3621';
+	branchCount++;
+	const treeGrowVal = (treeGrow > 1 ? 1 : treeGrow < 0.1 ? 0.1 : treeGrow) ** 2;
 
-    // Draw leaves on thinner branches
-    // if (width < 5) { // Increased threshold for more leaves
-    //     drawLeaf(endX, endY, dir, leafSize);
-    // }
-    
-    if (branchCount < maxBranches && leng > lenTwig && width > widthTwig) {
-        const rDir = randSI() ? -1 : 1;
-        treeGrow -= 0.2;
-        
-        drawBranch(
-            endX, endY,
-            dir + randS(angMin, angMax) * rDir,
-            leng * randS(lengMin, lengMax),
-            width * randS(widthMin, widthMax)
-        );
-        
-        drawBranch(
-            endX, endY,
-            dir + randS(angMin, angMax) * -rDir,
-            leng * randS(lengMin, lengMax),
-            width * randS(widthMin, widthMax)
-        );
+	const xx = Math.cos(dir) * leng * treeGrowVal;
+	const yy = Math.sin(dir) * leng * treeGrowVal;
+	const windSideWayForce = windX * yy - windY * xx;
 
-        if (randSI()) {
-            drawBranch(
-                endX, endY,
-                 leng * randS(lengMin, lengMax),
-                width * randS(widthMin, widthMax)
-            );
-        }
-        
-        treeGrow += 0.2;
-    }
+	dir +=
+		windStrength *
+		windActual *
+		(1 - width / maxTrunk) ** bendability *
+		windSideWayForce;
+
+	const endX = x + Math.cos(dir) * leng * treeGrowVal;
+	const endY = y + Math.sin(dir) * leng * treeGrowVal;
+	const branch = two.makeLine(x, y, endX, endY);
+	branch.linewidth = width;
+	branch.fill = "brown";
+	branch.stroke = "#4B3621";
+
+	// Draw leaves on thinner branches
+	// if (width < 5) { // Increased threshold for more leaves
+	//     drawLeaf(endX, endY, dir, leafSize);
+	// }
+
+	if (branchCount < maxBranches && leng > lenTwig && width > widthTwig) {
+		const rDir = randSI() ? -1 : 1;
+		treeGrow -= 0.2;
+
+		drawBranch(
+			endX,
+			endY,
+			dir + randS(angMin, angMax) * rDir,
+			leng * randS(lengMin, lengMax),
+			width * randS(widthMin, widthMax),
+		);
+
+		drawBranch(
+			endX,
+			endY,
+			dir + randS(angMin, angMax) * -rDir,
+			leng * randS(lengMin, lengMax),
+			width * randS(widthMin, widthMax),
+		);
+
+		if (randSI()) {
+			drawBranch(
+				endX,
+				endY,
+				leng * randS(lengMin, lengMax),
+				width * randS(widthMin, widthMax),
+			);
+		}
+
+		treeGrow += 0.2;
+	}
 }
 
 function updateWind() {
-    if (Math.random() < gustProbability) {
-        windCycleGustTime = (Math.random() * 10 + 1) | 0;
-    }
-    if (windCycleGustTime > 0) {
-        windCycleGustTime--;
-        windCycleGust += windCycleGustTime/20;
-    } else {
-        windCycleGust *= 0.99;
-    }        
-    windCycle += windCycleGust;
-    currentWind = (Math.sin(windCycle/40) * 0.6 + 0.4) ** 2;
-    currentWind = currentWind < 0 ? 0 : currentWind;
-    windFollow += (currentWind - windActual) * windBendRectSpeed;
-    windFollow *= windBranchSpring;
-    windActual += windFollow;
+	if (Math.random() < gustProbability) {
+		windCycleGustTime = (Math.random() * 10 + 1) | 0;
+	}
+	if (windCycleGustTime > 0) {
+		windCycleGustTime--;
+		windCycleGust += windCycleGustTime / 20;
+	} else {
+		windCycleGust *= 0.99;
+	}
+	windCycle += windCycleGust;
+	currentWind = (Math.sin(windCycle / 40) * 0.6 + 0.4) ** 2;
+	currentWind = currentWind < 0 ? 0 : currentWind;
+	windFollow += (currentWind - windActual) * windBendRectSpeed;
+	windFollow *= windBranchSpring;
+	windActual += windFollow;
 }
 
 // Don’t forget to tell two to draw everything to the screen
-two.bind('update', update)
+two.bind("update", update);
 two.play();
 
 function update() {
-    two.clear();
-    // updateWind();
-    drawTree(treeSeed);
+	two.clear();
+	// updateWind();
+	drawTree(treeSeed);
 }
 
 window.addEventListener("click", () => {
-    console.log('click!');
-    console.log("branchCount: ", branchCount)
-    console.log(randSI())
-    treeSeed = Math.random() * 10000 | 0;
-    treeGrow = 2;
-    console.log("new seed: ", treeSeed);
+	console.log("click!");
+	console.log("branchCount: ", branchCount);
+	console.log(randSI());
+	treeSeed = (Math.random() * 10000) | 0;
+	treeGrow = 2;
+	console.log("new seed: ", treeSeed);
 });
--- src/tree/tree_two_regen.js
+++ src/tree/tree_two_regen.js
@@ -1,20 +1,22 @@
-import getSeededRandom from './seededRandom.js';
+import getSeededRandom from "./seededRandom.js";
 
 // Make an instance of two and place it on the page.
 const params = {
-    fullscreen: true,
-    autostart: true
+	type: Two.Types.canvas,
+	fullscreen: true,
+	autostart: true,
 };
-let elem = document.body;
-let two = new Two(params).appendTo(elem);
+const elem = document.body;
+const two = new Two(params).appendTo(elem);
 
-let rand = getSeededRandom('acba');
-const randSI = (min = 0, max = 1) => Math.floor(rand() * (max - min + 1)) + min;
-const randS = (min = 0, max = 1) => rand() * (max - min) + min;
+let rand = getSeededRandom("acba");
+const randInt = (min = 0, max = 1) =>
+	Math.floor(rand() * (max - min + 1)) + min;
+const randFloat = (min = 0, max = 1) => rand() * (max - min) + min;
 
 const leafTexture = two.makeTexture("./tree/leaf.png");
 
-const angMin = 0.01;
+const angMin = 0.2;
 const angMax = 0.7;
 const lengMin = 0.7;
 const lengMax = 0.8;
@@ -32,10 +34,10 @@ const widthTwig = 5;
 const windX = -1;
 const windY = 0;
 const bendability = 1;
-const windStrength = 0.01 * bendability * ((200 ** 2) / (two.height ** 2));
+const windStrength = 0.01 * bendability * (200 ** 2 / two.height ** 2);
 const windBendRectSpeed = 0.01;
 const windBranchSpring = 0.98;
-const gustProbability = 1/100;
+const gustProbability = 1 / 100;
 
 let windCycle = 0;
 let windCycleGust = 0;
@@ -43,143 +45,162 @@ let windCycleGustTime = 0;
 let currentWind = 0;
 let windFollow = 0;
 let windActual = 0;
-let treeSeed = Math.random() * 10000 | 0;
+const treeSeed = (Math.random() * 10000) | 0;
 let branchCount = 0;
 let maxTrunk = 0;
 let treeGrow = 0.1;
-let done = 0;
+const done = 0;
 const leaves = two.makeGroup();
 const branches = two.makeGroup();
 
+const baseLeaf = two.makeSprite(leafTexture, 0, 0);
+
 function makeLeaf(x, y, dir, size) {
-    const actualSize = size * (1 + randS(-leafVariation, leafVariation));
-    // let leaf = new Two.Sprite(leafTexture, x, y, 0, 0, 60);
-    let leaf = two.makeSprite(leafTexture, x, y, 0, 0, 60);
-    leaves.add(leaf);
-    leaf.rotation = (dir + Math.PI / 2 + randS(-1, 1)); // Add slight random rotation
-    leaf.scale = actualSize / 20;
+	const actualSize = size * (1 + randFloat(-leafVariation, leafVariation));
+	// let leaf = new Two.Sprite(leafTexture, x, y);
+	const leaf = Object.assign({}, baseLeaf);
+	leaf.x = x;
+	leaf.y = y;
+	// leaves.add(leaf);
+	leaf.rotation = dir + Math.PI / 2 + randFloat(-1, 1); // Add slight random rotation
+	leaf.scale = actualSize / 20;
 }
 
 function drawTree(seed) {
-    branchCount = 0;
-    treeGrow += 0.02;
-    rand = getSeededRandom(seed.toString());
-    maxTrunk = randSI(trunkMin, trunkMax);
-    if (done) {
-        
-    }
-    makeBranches(two.width / 2, two.height, -Math.PI / 2, two.height / 4, maxTrunk);
+	branchCount = 0;
+	treeGrow += 0.02;
+	rand = getSeededRandom(seed.toString());
+	maxTrunk = randInt(trunkMin, trunkMax);
+	// if (done) {
+
+	// }
+	makeBranches(
+		two.width / 2,
+		two.height,
+		-Math.PI / 2,
+		two.height / 4,
+		maxTrunk,
+	);
 }
 
-
 function makeBranches(x, y, dir, leng, width) {
-    branchCount++;
-    const treeGrowVal = (treeGrow > 1 ? 1 : treeGrow < 0.1 ? 0.1 : treeGrow) ** 2;
-    
-    // const xx = Math.cos(dir) * leng * treeGrowVal;
-    // const yy = Math.sin(dir) * leng * treeGrowVal;
-    // const windSideWayForce = windX * yy - windY * xx;
-    
-    // dir += (windStrength * windActual) * ((1 - width / maxTrunk) ** bendability) * windSideWayForce;
-    
-    const endX = x + Math.cos(dir) * leng * treeGrowVal;
-    const endY = y + Math.sin(dir) * leng * treeGrowVal;
-    var branch = two.Line(x, y, endX, endY)
-    branch.linewidth = width;
-    branch.fill = 'brown';
-    branch.stroke = '#4B3621';
-    branches.add(branch);
+	branchCount++;
+	const treeGrowVal = (treeGrow > 1 ? 1 : treeGrow < 0.1 ? 0.1 : treeGrow) ** 2;
 
-    // Draw leaves on thinner branches
-    // if (width < 5) { // Increased threshold for more leaves
-    //     makeLeaves(endX, endY, dir, leafSize);
-    // }
-    
-    if (branchCount < maxBranches && leng > lenTwig && width > widthTwig) {
-        const rDir = randSI() ? -1 : 1;
-        treeGrow -= 0.2;
-        
-        makeBranches(
-            endX, endY,
-            dir + randS(angMin, angMax) * rDir,
-            leng * randS(lengMin, lengMax),
-            width * randS(widthMin, widthMax)
-        );
-        
-        makeBranches(
-            endX, endY,
-            dir + randS(angMin, angMax) * -rDir,
-            leng * randS(lengMin, lengMax),
-            width * randS(widthMin, widthMax)
-        );
+	const xx = Math.cos(dir) * leng * treeGrowVal;
+	const yy = Math.sin(dir) * leng * treeGrowVal;
+	const windSideWayForce = windX * yy - windY * xx;
 
-        if (randSI()) {
-            makeBranches(
-                endX, endY,
-                dir,
-                leng * randS(lengMin, lengMax),
-                width * randS(widthMin, widthMax)
-            );
-        }
-        
-        treeGrow += 0.2;
-    }
+	dir +=
+		windStrength *
+		windActual *
+		(1 - width / maxTrunk) ** bendability *
+		windSideWayForce;
+
+	const endX = x + Math.cos(dir) * leng * treeGrowVal;
+	const endY = y + Math.sin(dir) * leng * treeGrowVal;
+	// let branch = new Two.Line(x, y, endX, endY)
+	const branch = two.makeLine(x, y, endX, endY);
+	branch.linewidth = width;
+	branch.fill = "brown";
+	branch.stroke = "#4B3621";
+	// branches.add(branch);
+
+	// Draw leaves on thinner branches
+	if (width < 5) {
+		// Increased threshold for more leaves
+		makeLeaf(endX, endY, dir, leafSize);
+	}
+
+	if (branchCount < maxBranches && leng > lenTwig && width > widthTwig) {
+		const rDir = randInt() ? -1 : 1;
+		treeGrow -= 0.2;
+
+		makeBranches(
+			endX,
+			endY,
+			dir + randFloat(angMin, angMax) * rDir,
+			leng * randFloat(lengMin, lengMax),
+			width * randFloat(widthMin, widthMax),
+		);
+
+		makeBranches(
+			endX,
+			endY,
+			dir + randFloat(angMin, angMax) * -rDir,
+			leng * randFloat(lengMin, lengMax),
+			width * randFloat(widthMin, widthMax),
+		);
+
+		if (randInt()) {
+			makeBranches(
+				endX,
+				endY,
+				dir,
+				leng * randFloat(lengMin, lengMax),
+				width * randFloat(widthMin, widthMax),
+			);
+		}
+
+		treeGrow += 0.2;
+	}
 }
 
 function updateWind() {
-    if (Math.random() < gustProbability) {
-        windCycleGustTime = (Math.random() * 10 + 1) | 0;
-    }
-    if (windCycleGustTime > 0) {
-        windCycleGustTime--;
-        windCycleGust += windCycleGustTime/20;
-    } else {
-        windCycleGust *= 0.99;
-    }        
-    windCycle += windCycleGust;
-    currentWind = (Math.sin(windCycle/40) * 0.6 + 0.4) ** 2;
-    currentWind = currentWind < 0 ? 0 : currentWind;
-    windFollow += (currentWind - windActual) * windBendRectSpeed;
-    windFollow *= windBranchSpring;
-    windActual += windFollow;
+	if (Math.random() < gustProbability) {
+		windCycleGustTime = (Math.random() * 10 + 1) | 0;
+	}
+	if (windCycleGustTime > 0) {
+		windCycleGustTime--;
+		windCycleGust += windCycleGustTime / 20;
+	} else {
+		windCycleGust *= 0.99;
+	}
+	windCycle += windCycleGust;
+	currentWind = (Math.sin(windCycle / 40) * 0.6 + 0.4) ** 2;
+	currentWind = currentWind < 0 ? 0 : currentWind;
+	windFollow += (currentWind - windActual) * windBendRectSpeed;
+	windFollow *= windBranchSpring;
+	windActual += windFollow;
 }
 
 // Don’t forget to tell two to draw everything to the screen
-// two.bind('update', update)
-// two.play();
-//
-// function update() {
-//     two.clear();
-//     updateWind();
-//     drawTree(treeSeed);
-// }
+two.bind("update", update);
+two.play();
+
+function update() {
+	two.clear();
+	updateWind();
+	drawTree(treeSeed);
+}
 // drawTree(treeSeed);
 
-function drawLeaves() {
-    for (let i = 0; i < 200; i++) {
-        let x = randSI(0, two.width);
-        let y = randSI(0, two.height);
-        let dir = randS(0, Math.PI*2);
-        makeLeaf(x, y, dir, leafSize);
-    }
+function scatterLeaves() {
+	for (let i = 0; i < 200; i++) {
+		const x = randInt(0, two.width);
+		const y = randInt(0, two.height);
+		const dir = randFloat(0, Math.PI * 2);
+		makeLeaf(x, y, dir, leafSize);
+	}
 }
 
-drawLeaves();
-// setTimeout(function() { debugger; }, 5000)
+// function moveLeaves() {
+//     for (leaf in leaves) {
 
-// function renderLeaves() {
-//     for (leaf in leaves.Children) {
-        
 //     }
-
 // }
 
-// window.addEventListener("click", () => {
-//     console.log('click!');
-//     console.log("branchCount: ", branchCount)
-//     console.log(randSI())
-//     treeSeed = Math.random() * 10000 | 0;
-//     treeGrow = 2;
-//     console.log("new seed: ", treeSeed);
-//     console.log("num branches group: ", branches.children.length)
-// });
+// scatterLeaves();
+// two.update();
+
+// setTimeout(function() { debugger; }, 5000)
+
+window.addEventListener("click", () => {
+	// console.log('click!');
+	// console.log("branchCount: ", branchCount)
+	// console.log(randInt())
+	// treeSeed = Math.random() * 10000 | 0;
+	treeGrow += 0.2;
+	// console.log("new seed: ", treeSeed);
+	// console.log("num branches group: ", branches.children.length)
+});
